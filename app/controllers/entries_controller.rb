35e5646542e82316207409345721adc1bb5e12d8
Austin Stern <austin.stern@kellogg.northwestern.edu>
2025-05-19 20:27:49 +0000

    pasted code into controllers

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,21 @@
 class EntriesController < ApplicationController
+  def new
+    @entry = Entry.new
+    @place_id = params[:place_id]
+    render({ :template => "entries/new.html.erb" })
+  end
+  
+  def create
+    @entry = Entry.new
+    @entry.title = params[:title]
+    @entry.description = params[:description]
+    @entry.posted_on = params[:posted_on]
+    @entry.place_id = params[:place_id]
+    
+    if @entry.save
+      redirect_to("/places/#{@entry.place_id}")
+    else
+      render({ :template => "entries/new.html.erb" })
+    end
+  end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,27 @@
 class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+    render({ :template => "places/index.html.erb" })
+  end
+  
+  def show
+    @place = Place.find(params[:id])
+    render({ :template => "places/show.html.erb" })
+  end
+  
+  def new
+    @place = Place.new
+    render({ :template => "places/new.html.erb" })
+  end
+  
+  def create
+    @place = Place.new
+    @place.name = params[:name]
+    
+    if @place.save
+      redirect_to("/places")
+    else
+      render({ :template => "places/new.html.erb" })
+    end
+  end
 end
